24d19b980e2e4865a56252fc8e8ff13e2d8bff4c
wilber jose matus <wilberj1987@gmail.com>
2023-10-09 21:39:01 -0600

    profiles-ext/i-sadav#1766 ajustes en diseño general landing

--- CAPA_DATOS/EntityStructure/FilterData.cs
+++
@@ -1,9 +0,0 @@
-namespace CAPA_DATOS
-{
-    public class FilterData
-    {
-       public string? PropName { get; set; }
-       public string? FilterType { get; set; }
-       public List<String?>? Values { get; set; }
-    }
-}
